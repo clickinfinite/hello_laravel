17d828974b0d876934e123f9981c4ddb3d58588b
clickinifine <trafficbeast@outlook.com>
2017-08-13 10:21:20 +0000

    add,

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello xiaoBaiLong!</div>
+                <div class="title">Hello, xiaoBaiLong!</div>
             </div>
         </div>
     </body>
